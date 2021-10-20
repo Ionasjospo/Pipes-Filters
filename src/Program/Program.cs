4053ff8a30e506f40ff30bb0ca1c5b3e506bf585
Ionasjospo <88661713+Ionasjospo@users.noreply.github.com>
2021-10-20 02:15:34 -0300

    :(

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,26 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+
+            
+            IFilter grey = new FilterGreyscale();
+            IFilter negative = new FilterNegative();
+            PipeNull pnull = new PipeNull();
+            PipeSerial segpipe = new PipeSerial(negative, pnull);
+            PipeSerial pipe = new PipeSerial(grey, segpipe);
+            picture = pipe.Send(picture);
+
+
+           
+            PictureProvider applyfilters = new PictureProvider();
+            FilterSave luke_ = applyfilters.toSavePicture(applyfilters , @"luke.jpg");
+            PipeSerial terpipe = new PipeSerial(FilterSave, pnull);
+
+        
+
+
         }
     }
 }
